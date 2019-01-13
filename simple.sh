9a45c200afbda396a7085a2b9d103d81a5b8fb75
Jan Nieuwenhuizen <janneke@gnu.org>
2019-01-13 11:45:35 +0100

    simple.sh: Add bit more elaborate test.  TODO: import/commit scm sources?

--- simple.sh
+++ simple.sh
@@ -11,8 +11,16 @@ echo '(display "hello\n")' | MES_BOOT=boot-01.scm out-glibc/mes
 # have a recent mes @wip worktree checked-out in ../mes
 test -d ../mes && test -f ../mes/simple.sh
 
-# A somewhat more elaborate test, both should work
+# A somewhat more elaborate test: running from ../mes
 ( cd ../mes && MES_DEBUG=4 MES_PREFIX=mes MES=../mes-m2/out-glibc/mes tests/base.test )
 
+# Or run from here
+rm -rf mes module tests
 ln -sf ../mes/{mes,module,tests} .
+MES_DEBUG=4 MES_PREFIX=mes MES=out-glibc/mes tests/base.test
+
+# Or copy over the necessary files (and commit them here?)
+rm -rf mes module tests
+tar -cf- -C ../mes mes/module/mes/{boot-03.scm,{type-0,module,base,quasiquote,let,scm}.mes} | tar -xf-
+tar -cf- -C ../mes module/mes/quasiquote.mes module/mes/test.scm tests/base.test tests/data | tar -xf-
 MES_DEBUG=4 MES_PREFIX=mes MES=out-glibc/mes tests/base.test
