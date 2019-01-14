612b6ff1f834b0eeb92f8821aa4b6ea7fb731722
Jeremiah Orians <jeremiah@pdp10.guru>
2019-01-13 20:41:14 -0500

    Further reductions in dependency graph

--- mes_posix.c
+++ mes_posix.c
@@ -1,6 +1,7 @@
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
  * Copyright © 2016,2017,2018 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ *  Copyright © 2019 Jeremiah Orians
  *
  * This file is part of GNU Mes.
  *
@@ -44,7 +45,24 @@
 #define MAKE_STRING0(x) make_string (x, strlen (x))
 #define MAKE_STRING_PORT(x) make_cell__ (TPORT, -length__ (g_ports) - 2, x)
 
-int mes_open (char const *file_name, int flags, ...);
+void __ungetc_init();
+// The Mes C Library defines and initializes these in crt1
+int mes_open(char const *file_name, int flags, ...)
+{
+	va_list ap;
+	va_start(ap, flags);
+	int mask = va_arg(ap, int);
+	__ungetc_init();
+	int r = open(file_name, flags, mask);
+
+	if(r > 2)
+	{
+		__ungetc_buf[r] = -1;
+	}
+
+	va_end(ap);
+	return r;
+}
 #define open mes_open
 
 
@@ -65,6 +83,123 @@ SCM error(SCM key, SCM x);
 int eputs(char const* s);
 char *itoa (int number);
 SCM acons (SCM key, SCM value, SCM alist);
+
+int eputs(char const* s)
+{
+	int i = strlen(s);
+	write(__stderr, s, i);
+	return 0;
+}
+
+char* ntoab(long x, int base, int signed_p)
+{
+	static char itoa_buf[20];
+	char *p = itoa_buf + 11;
+	*p-- = 0;
+	int sign_p = 0;
+	unsigned long u = x;
+
+	if(signed_p && x < 0)
+	{
+		sign_p = 1;
+		u = -x;
+	}
+
+	do
+	{
+		long i = u % base;
+		*p-- = i > 9 ? 'a' + i - 10 : '0' + i;
+		u = u / base;
+	} while(u);
+
+	if(sign_p && *(p + 1) != '0')
+	{
+		*p-- = '-';
+	}
+
+	return p + 1;
+}
+
+char* itoa(int x)
+{
+	return ntoab(x, 10, 1);
+}
+
+void __ungetc_init()
+{
+	if(__ungetc_buf[RLIMIT_NOFILE] == 0)
+	{
+		memset(__ungetc_buf, -1, (RLIMIT_NOFILE + 1)*sizeof(int));
+	}
+}
+
+int fdgetc(int fd)
+{
+	__ungetc_init();
+	char c;
+	int i = __ungetc_buf[fd];
+
+	if(i >= 0)
+	{
+		__ungetc_buf[fd] = -1;
+	}
+	else
+	{
+		int r = read(fd, &c, 1);
+
+		if(r < 1)
+		{
+			return -1;
+		}
+
+		i = c;
+	}
+
+	if(i < 0)
+	{
+		i += 256;
+	}
+
+	return i;
+}
+
+int fdputc(int c, int fd)
+{
+	write(fd, (char*)&c, 1);
+	return 0;
+}
+
+int fdputs(char const* s, int fd)
+{
+	int i = strlen(s);
+	write(fd, s, i);
+	return 0;
+}
+
+int fdungetc(int c, int fd)
+{
+	__ungetc_init();
+
+	if(c == -1)
+	{
+		return c;
+	}
+	else if(__ungetc_buf[fd] != -1)
+	{
+		eputs(" ***MES C LIB*** fdungetc ungetc buffer overflow fd=");
+		eputs(itoa(fd));
+		eputs("\n");
+		exit(1);
+	}
+
+	__ungetc_buf[fd] = c;
+	return c;
+}
+
+int _fdungetc_p(int fd)
+{
+	return __ungetc_buf[fd] >= 0;
+}
 
 int peekchar()
 {
