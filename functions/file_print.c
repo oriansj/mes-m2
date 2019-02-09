99019697584e93ae0ca0fd40aaf67994a726a7d0
Jeremiah Orians <jeremiah@pdp10.guru>
2019-02-09 17:34:33 -0500

    Leveraging char_lookup

--- functions/file_print.c
+++ functions/file_print.c
@@ -22,6 +22,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// CONSTANT TRUE 1
+#define TRUE 1
+// CONSTANT FALSE 0
+#define FALSE 0
+
 void file_print(char* s, FILE* f)
 {
 	while(0 != s[0])
@@ -52,24 +57,57 @@ int fdputs(char const* s, int fd)
 	return 0;
 }
 
+int char2hex(int c);
+char block[4];
+char* char_lookup(int c, int type)
+{
+	static char* s = block;
+	s[0] = '\\';
+	if(type)
+	{
+		if(c == '\0') return "\\nul";
+		else if(c == '\a') return "\\alarm";
+		else if(c == '\b') return "\\backspace";
+		else if(c == '\t') return "\\tab";
+		else if(c == '\n') return "\\newline";
+		else if(c == '\v') return "\\vtab";
+		else if(c == '\f') return "\\page";
+		else if(c == '\r') return "\\return";
+		else if(c == ' ') return "\\space";
+		else
+		{
+			s[1] = char2hex((c & 0xF0) >> 4);
+			s[2] = char2hex(c & 0xF);
+		}
+	}
+	else
+	{
+		s[2] = 0;
+		if(c == '\0') s[1] = '0';
+		else if(c == '\a') s[1] = 'a';
+		else if(c == '\b') s[1] = 'b';
+		else if(c == '\t') s[1] = 't';
+		else if(c == '\v') s[1] = 'v';
+		else if(c == '\n') s[1] = 'n';
+		else if(c == '\f') s[1] = 'f';
+		else if(c == '\r') s[1] = 'r';
+		else if(c == '\e') s[1] = 'e';
+		else if(c == '\\') s[1] = '\\';
+		else if(c == '"') s[1] = '"';
+		else
+		{
+			s[0] = c;
+			s[1] = 0;
+		}
+	}
+	return s;
+}
+
 void raw_print(char* s, int fd)
 {
-	long v = s[0];
-	while(0 != v)
+	while(0 != s[0])
 	{
-		if(v == '\0') fd_print("\\0", fd);
-		else if(v == '\a') fd_print("\\a", fd);
-		else if(v == '\b') fd_print("\\b", fd);
-		else if(v == '\t') fd_print("\\t", fd);
-		else if(v == '\v') fd_print("\\v", fd);
-		else if(v == '\n') fd_print("\\n", fd);
-		else if(v == '\f') fd_print("\\f", fd);
-		else if(v == '\r') fd_print("\\r", fd);
-		else if(v == '\e') fd_print("\\e", fd);
-		else if(v == '\\') fd_print("\\\\", fd);
-		else if(v == '"') fd_print("\\\"", fd);
-		else fdputc(v, fd);
+		fd_print(char_lookup(s[0], FALSE), fd);
 		s = s + 1;
-		v = s[0];
 	}
 }
