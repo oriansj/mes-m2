78ae3c569ad4a16738927b8833506f82d67ca11b
Jeremiah Orians <jeremiah@pdp10.guru>
2021-01-10 08:38:40 -0500

    Putting the hack back in for now

--- mes_cell.c
+++ mes_cell.c
@@ -758,9 +758,11 @@ struct cell* make_record(struct cell* type, struct cell* vector)
 
 struct cell* cell_invoke_function(struct cell* cell, struct cell* vals)
 {
+// /*
 #if __MESC__
 	struct cell* (*fp)(struct cell*) = cell->function;
 #else
+// */
 	FUNCTION* fp = cell->function;
 #endif
 	return fp(vals);
